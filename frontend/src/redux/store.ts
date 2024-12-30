b54678671835c0e5f9150280f622ec61ce633277
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-12-30 18:54:30 +0530

    Configure store for teacher api

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { studentApi } from "./api/studentApi";
 import { classroomApi } from "./api/classroomApi";
+import { teacherApi } from "./api/teacher.Api";
 
 export const store = configureStore({
   reducer: {
     [studentApi.reducerPath]: studentApi.reducer,
     [classroomApi.reducerPath]: classroomApi.reducer,
+    [teacherApi.reducerPath]: teacherApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       studentApi.middleware,
-      classroomApi.middleware
+      classroomApi.middleware,
+      teacherApi.middleware
     ),
 });
 
